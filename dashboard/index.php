dde4ed8fee42583de98c61ab587c2c0dfdbcef19
Arsyan Ismail <arsyan@arsyan.com>
2014-07-02 01:56:06 +0800

    change 30-10

--- dashboard/index.php
+++ dashboard/index.php
@@ -137,13 +137,13 @@ function update()
 $( document ).ready(function() {
 	//start
 	update();
-	setInterval("update()",15000);
+	setInterval("update()",30000);
 
 	gauge1.init();
 	gauge2.init(); 
 	gauge3.init(); 
 	gauge4.init(); 	
-	setInterval("plusMinus()",2000);
+	setInterval("plusMinus()",10000);
 	//end
 });
 </script>
